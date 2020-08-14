76a35ea950e491c0a342598c80e32a1b9b23c071
shipeng <18872593573@163.com>
2020-08-14 12:00:53 +0800

    新增集成rabbitmq

--- service/src/main/java/com/test/demo/TestController.java
+++ service/src/main/java/com/test/demo/TestController.java
@@ -12,9 +12,11 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
+import java.net.*;
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
@@ -53,10 +55,10 @@ public class TestController {
             hashmap.put(s,request.getHeader(s));
 
         }
-        String header = request.getRemoteAddr();
-        String remoteHost = request.getRemoteHost();
-        hashmap.put("remoteHost",remoteHost);
-        hashmap.put("header",header);
+        String ip = request.getRemoteAddr();
+        String localMac = getLocalMac(ip);
+        hashmap.put("ip",ip);
+        hashmap.put("localMac",localMac);
         JSONObject jsonObject = new JSONObject(hashmap);
         return jsonObject;
     }
@@ -167,5 +169,58 @@ public class TestController {
             }
         }
         return ret.replace("+", "%20");
+    }
+
+    public  String getLocalMac(String ipAddress) throws SocketException, UnknownHostException {
+        // TODO Auto-generated method stub
+        String str = "";
+        String macAddress = "";
+        final String LOOPBACK_ADDRESS = "127.0.0.1";
+        // 如果为127.0.0.1,则获取本地MAC地址。
+        if (LOOPBACK_ADDRESS.equals(ipAddress)) {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            // 貌似此方法需要JDK1.6。
+            byte[] mac = NetworkInterface.getByInetAddress(inetAddress)
+                    .getHardwareAddress();
+            // 下面代码是把mac地址拼装成String
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < mac.length; i++) {
+                if (i != 0) {
+                    sb.append("-");
+                }
+                // mac[i] & 0xFF 是为了把byte转化为正整数
+                String s = Integer.toHexString(mac[i] & 0xFF);
+                sb.append(s.length() == 1 ? 0 + s : s);
+            }
+            // 把字符串所有小写字母改为大写成为正规的mac地址并返回
+            macAddress = sb.toString().trim().toUpperCase();
+            return macAddress;
+        } else {
+            // 获取非本地IP的MAC地址
+            try {
+                System.out.println(ipAddress);
+                Process p = Runtime.getRuntime()
+                        .exec("nbtstat -A " + ipAddress);
+                System.out.println("===process==" + p);
+                InputStreamReader ir = new InputStreamReader(p.getInputStream());
+
+                BufferedReader br = new BufferedReader(ir);
+
+                while ((str = br.readLine()) != null) {
+                    if (str.indexOf("MAC") > 1) {
+                        macAddress = str.substring(str.indexOf("MAC") + 9,
+                                str.length());
+                        macAddress = macAddress.trim();
+                        System.out.println("macAddress:" + macAddress);
+                        break;
+                    }
+                }
+                p.destroy();
+                br.close();
+                ir.close();
+            } catch (IOException ex) {
+            }
+            return macAddress;
+        }
     }
 }
