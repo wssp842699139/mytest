c2d99c87529bd013f2dea601176cdf012f57ce87
shipeng <18872593573@163.com>
2020-08-17 17:59:22 +0800

    修改服务器名字

---
+++ user/src/main/java/com/test/demo/Test2.java
@@ -0,0 +1,20 @@
+package com.test.demo;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+
+/**
+ * @version V1.0
+ * @Author ship
+ * @Date 2020/7/6 19:45
+ * @package com.test.demo
+ */
+public class Test2 {
+
+    public static void main(String[] args) throws SocketException, UnknownHostException {
+
+    }
+
+
+
+}
